d2b46ee03814a9a370fb570fa06b1dc60cc1a702
azhi <astzhe@gmail.com>
2020-06-12 15:15:47 +0300

    rename to CoAP parser, remove all client/server stuff

--- mix.exs
+++ mix.exs
@@ -1,33 +1,24 @@
-defmodule Coap.MixProject do
+defmodule CoAP.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :coap_ex,
+      app: :coap_parser,
       version: "0.1.0",
-      elixir: "~> 1.6",
-      start_permanent: Mix.env() == :prod,
+      elixir: "~> 1.10",
       deps: deps()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      mod: {CoAP, []},
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug, "~> 1.0"},
-      {:gen_coap, github: "gotthardp/gen_coap", only: [:dev, :test]},
-      {:stream_data, "~> 0.1", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:telemetry, "~> 0.4.0"}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 end
--- test/coap/phoenix/request_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule CoAP.Phoenix.RequestTest do
-  use ExUnit.Case
-  doctest CoAP.Phoenix.Request
-end
